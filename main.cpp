a095123985a50ee4204209d59811bfe229de9d73
Giveonaldo <giveonaldo@gmail.com>
2022-12-13 14:12:52 +0700

    Code V1.1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+using std::ofstream;
 
 int main()
 {
+	string username, password, file_name;
+	cout << "<-----Create a User File----->" << endl;
+	cout << "Enter file name: ";
+	cin >> file_name;
+	cout << "Enter username: ";
+	cin >> username;
+	cout << "Enter password: ";
+	cin >> password;
 
 	return 0;
 }
